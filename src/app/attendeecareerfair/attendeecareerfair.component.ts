8cdbac7f3c2412cbb0e041f841b99a7adcf7a6f2
amill238 <35568796+amill238@users.noreply.github.com>
2019-11-24 23:35:50 -0500

    final sprint 3 changes

--- src/app/attendeecareerfair/attendeecareerfair.component.ts
+++ src/app/attendeecareerfair/attendeecareerfair.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 //imports for dialog box
 import { MatDialog } from '@angular/material';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Component({
@@ -12,18 +13,24 @@ import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation
   styleUrls: ['./attendeecareerfair.component.css']
 })
 export class AttendeecareerfairComponent implements OnInit {
-
+  private cookieValue: string;
   careerFairs: CareerFair[]
 
 
   constructor(private httpClientService:HttpClientService,
-    public dialog: MatDialog) { }
+    public dialog: MatDialog, private cookieService: CookieService) { }
+
 
   ngOnInit() {    
+    // get the cookie for user ID 
+    this.cookieValue = this.cookieService.get('userID');
+    
     var observable: Observable<CareerFair[]>;
     observable = this.httpClientService.getCareerFair();
     observable.subscribe(response => this.careerFairs = response);
   }
+
+
   //constructor(public dialog: MatDialog) {}
   returnResult ="";
   openDialog(): void {
